96fa2fe5873565c219c31bad70719c0a836e48be
Healm <116085297+Agent-Healm@users.noreply.github.com>
2024-06-08 21:39:07 +0700

    Add files via upload

---
+++ inventorist3/src/main/java/source/InputMenuController.java
@@ -0,0 +1,74 @@
+package source;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class InputMenuController {
+    @FXML private ImageView imageViewPreview;
+    @FXML private TextField textFieldNama;
+    @FXML private TextField textFieldKuantitas;
+    @FXML private TextField textFieldImagePath;
+    @FXML private TextField textFieldTipe;
+    @FXML private Label labelNama;
+    @FXML private Label labelKuantitas;
+    @FXML private Label labelImagePath;
+    @FXML private Label labelTipe;
+    @FXML private Button buttonPreviewImage ;
+    @FXML private Button buttonSubmit;
+    
+    FileInputStream fin;
+
+    @FXML private void actionSubmit() throws IOException{
+        if(textFieldNama.getLength() == 0){ return; }
+        if(textFieldKuantitas.getLength() == 0){ return; }
+        if(textFieldImagePath.getLength() == 0){ return; }
+        if(textFieldTipe.getLength() == 0){ return; }
+
+        Barang barang = new Barang(textFieldNama, 
+        textFieldKuantitas, 
+        textFieldImagePath, 
+        textFieldTipe);
+
+        fin = new FileInputStream(barang.getImagePath());
+        
+        try {
+            PreparedStatement psmt = Db.connection.prepareStatement(
+                "INSERT INTO inventory (nama, kuantitas, tipe, image) VALUES (?, ?, ?, ?);"
+            );
+            psmt.setString(1, barang.getNama());
+            psmt.setInt(2, barang.getKuantitas());
+            psmt.setString(3, barang.getTipe());
+            psmt.setBlob(4, fin);
+            
+            psmt.executeUpdate();
+        }
+        catch (SQLException sql){
+            System.out.println(sql.getLocalizedMessage());
+        }
+
+        App.setRoot("menu");
+    }
+
+    @FXML private void actionImagePath() throws IOException, SQLException {
+        try {
+            imageViewPreview.setImage(new Image(new FileInputStream(textFieldImagePath.getText())));
+            imageViewPreview.setFitHeight(300);
+            imageViewPreview.setPreserveRatio(true);
+        }
+        catch (FileNotFoundException fileError) {
+            System.out.println(fileError.getLocalizedMessage());
+            System.out.println("Where File hm? ");
+        }
+    }
+}
---
+++ inventorist3/src/main/java/source/MenuController.java
@@ -0,0 +1,95 @@
+package source;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.sql.Blob;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class MenuController {
+    @FXML private Button buttonCreate;
+    @FXML private TableView<Barang> tableViewBarang;
+
+    private ObservableList<TableColumn<Barang, ?>> cols = FXCollections.observableArrayList();
+    private static String cd = System.getProperty("user.dir");
+
+
+    @SuppressWarnings("unchecked")
+    public void initialize() throws IOException, SQLException{
+        TableColumn<Barang, String> colNama = new TableColumn<Barang, String>("Nama");
+        TableColumn<Barang, Integer> colKuantitas = new TableColumn<Barang, Integer>("Qty");
+        TableColumn<Barang, ImageView> colImage = new TableColumn<Barang, ImageView>("Gambar");
+        TableColumn<Barang, String> colTipe = new TableColumn<Barang, String>("Tipe");
+
+        colNama.setCellValueFactory(new PropertyValueFactory<Barang, String>("nama"));
+        colKuantitas.setCellValueFactory(new PropertyValueFactory<Barang, Integer>("kuantitas"));
+        colImage.setCellValueFactory(new PropertyValueFactory<Barang, ImageView>("imageView"));
+        colTipe.setCellValueFactory(new PropertyValueFactory<Barang, String>("tipe"));
+
+        cols.addAll(
+            colNama,
+            colKuantitas,
+            colImage,
+            colTipe
+        );  
+
+        tableViewBarang.getColumns().addAll(cols);
+        tableViewBarang.getItems().addAll(loadDb());
+    }
+
+    private static ObservableList<Barang> loadDb() throws SQLException, IOException{
+        ObservableList<Barang> daftarBarang = FXCollections.observableArrayList();
+        String cdImage = cd + "/src/main/resources/images/item thumbnails/";
+        String pathToImage;
+
+        PreparedStatement psmt = Db.connection.prepareStatement(
+            "SELECT nama, kuantitas, image, tipe FROM inventory;");
+        ResultSet rs = psmt.executeQuery();
+
+        while (rs.next()){
+            String barangNama = rs.getString("nama");
+            int barangKuantitas = rs.getInt("kuantitas");
+            Blob barangImage = rs.getBlob("image");
+            String barangTipe = rs.getString("tipe");
+
+            byte barr[] = barangImage.getBytes(1, (int) barangImage.length());
+
+            pathToImage = cdImage + barangNama + ".jpg";
+            File imageFile = new File(pathToImage);
+            if (!imageFile.exists()){
+                FileOutputStream fout = new FileOutputStream(pathToImage);
+                fout.write(barr);
+                fout.close();
+            }
+
+            Barang barang = new Barang(barangNama, barangKuantitas, "none", barangTipe);
+            ImageView imageViewPreview = new ImageView(new Image(new FileInputStream(pathToImage)));
+            imageViewPreview.setPreserveRatio(true);
+            imageViewPreview.setFitHeight(100);
+            
+            barang.setImageView(imageViewPreview);
+
+            daftarBarang.add(barang);
+        }
+
+        return daftarBarang;
+    }
+
+    @FXML private void actionCreate() throws IOException {
+        App.setRoot("inputmenu");
+    }   
+
+}
